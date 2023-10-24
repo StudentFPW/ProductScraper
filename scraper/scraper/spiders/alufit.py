f5ffd16c6552b2f2c4c054132dc0d42af6f10b83
StudentFPW <jofeleonids00@gmail.com>
2023-10-24 22:27:27 +0300

    24/10/2023

--- scraper/scraper/spiders/alufit.py
+++ scraper/scraper/spiders/alufit.py
@@ -6,24 +6,46 @@ from scrapy.spiders import CrawlSpider, Rule
 class AlufitSpider(CrawlSpider):
     name = "alufit"
     allowed_domains = ["alufit.ru"]
-    start_urls = ["https://alufit.ru"]
+    start_urls = ["https://alufit.ru/katalog"]
 
-    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+    rules = (
+        Rule(
+            LinkExtractor(allow=r"gotovyie-izdeliya"),
+            callback="parse_item",
+            follow=True,
+        ),
+    )
 
     def parse_item(self, response):
-        item = {}
-        item["Адрес страницы: "] = response.url
-        item["Название: "] = ""
-        item["Цена, руб: "] = ""
-        item["Ширина: "] = ""
-        item["Высота: "] = ""
-        item["Тип жалюзи: "] = ""
-        item["Цвет: "] = ""
-        item["Ширина ламели (см): "] = ""
-        item["Материал: "] = ""
-        item["Прозрачность: "] = ""
-        item["Открытие: "] = ""
-        item["Крепление: "] = ""
-        item["Автоматика: "] = ""
-        item["Раздел: "] = ""
-        return item
+        table = response.css("div.af-introtext.af-cell-half-width").get().strip()
+        return {"url": response.url, "data": table}
+
+
+# import re
+# pattern = re.compile("<.*?>")
+# if table is not None:
+#     item["table"] = (
+#         re.sub(
+#             pattern,
+#             "",
+#             response.css("div.af-introtext.af-cell-half-width").get(),
+#         )
+#         .replace("\n", " > ")
+#         .replace("\r", "")
+#     )
+# return {"table": item.get("table"), "url": response.url}
+
+# item["Адрес страницы: "] = response.url
+# item["Название: "] = ""
+# item["Цена, руб: "] = ""
+# item["Ширина: "] = ""
+# item["Высота: "] = ""
+# item["Тип жалюзи: "] = ""
+# item["Цвет: "] = ""
+# item["Ширина ламели (см): "] = ""
+# item["Материал: "] = ""
+# item["Прозрачность: "] = ""
+# item["Открытие: "] = ""
+# item["Крепление: "] = ""
+# item["Автоматика: "] = ""
+# item["Раздел: "] = ""
