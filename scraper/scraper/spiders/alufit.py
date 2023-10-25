12fc4903e616b047f1aaaac778c0bd3b02a746ed
StudentFPW <jofeleonids00@gmail.com>
2023-10-25 20:18:06 +0300

    25/10/2023

--- scraper/scraper/spiders/alufit.py
+++ scraper/scraper/spiders/alufit.py
@@ -17,25 +17,31 @@ class AlufitSpider(CrawlSpider):
     )
 
     def parse_item(self, response):
-        table = response.css("div.af-introtext.af-cell-half-width").get().strip()
-        return {"url": response.url, "data": table}
+        # response.css("table.table-product").getall() TABLE
+        # response.css("td.bb::text")[8].get().strip() FIELD
+        # response.css("td.nobb b::text")[8].get().strip() TEXT
+
+        item = {}
+        fields = response.css("td.bb::text").getall()
+        texts = response.css("td.nobb b::text").getall()
+
+        name = response.css("div.af-h1.af-h1-ha h1::text").get()
+        price = response.css("span.af-product-price__value span::text").get()
+        position = response.css("li.breadcrumb-item span::text")[2].get()
+
+        for key, value in zip(fields, texts):
+            if name or price or position is not None:
+                item["Название"] = name.strip()
+                item["Цена, руб"] = (
+                    price.strip() + " " + response.css("span.ruble::text").get().strip()
+                )
+                item["Раздел"] = position.strip()
+            item[key.strip()] = value.strip()
+
+        return {"Адрес страницы": response.url, "Данные": item}
 
 
-# import re
-# pattern = re.compile("<.*?>")
-# if table is not None:
-#     item["table"] = (
-#         re.sub(
-#             pattern,
-#             "",
-#             response.css("div.af-introtext.af-cell-half-width").get(),
-#         )
-#         .replace("\n", " > ")
-#         .replace("\r", "")
-#     )
-# return {"table": item.get("table"), "url": response.url}
-
-# item["Адрес страницы: "] = response.url
+# item["Адрес страницы: "] = ""
 # item["Название: "] = ""
 # item["Цена, руб: "] = ""
 # item["Ширина: "] = ""
